062b1ee231c93d8e4083d8e2b580d111ceb42f98
VajiraPrabuddhaka <vajiraprabuddhaka@gmail.com>
2025-08-18 12:02:13 +0530

    Create a HTTP client to call grpc server

--- client/main.go
+++ client/main.go
@@ -2,46 +2,67 @@ package main
 
 import (
 	"context"
-	"io"
+	"encoding/json"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
-	pb "greeter-grpc-byoc/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	pb "greeter-grpc-byoc/proto"
 )
 
+type GreetResponse struct {
+	Message string `json:"message"`
+}
+
+func greetHandler(client pb.GreetingServiceClient) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			http.Error(w, "name parameter is required", http.StatusBadRequest)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		resp, err := client.SayHello(ctx, &pb.HelloRequest{Name: name})
+		if err != nil {
+			log.Printf("gRPC call failed: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		response := GreetResponse{Message: resp.GetMessage()}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
 func main() {
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	target := os.Getenv("GRPC_SERVER_TARGET")
+	if target == "" {
+		target = "localhost:50051"
+	}
+
+	conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("did not connect to gRPC server: %v", err)
 	}
 	defer conn.Close()
 
-	c := pb.NewGreetingServiceClient(conn)
+	client := pb.NewGreetingServiceClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	http.HandleFunc("/greet", greetHandler(client))
 
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "World"})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.GetMessage())
-
-	stream, err := c.SayHelloStream(context.Background(), &pb.HelloRequest{Name: "Stream User"})
-	if err != nil {
-		log.Fatalf("could not get stream: %v", err)
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = "8080"
 	}
 
-	for {
-		response, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("could not receive: %v", err)
-		}
-		log.Printf("Stream Greeting: %s", response.GetMessage())
-	}
+	log.Printf("Starting HTTP server on port %s", port)
+	log.Printf("gRPC backend: %s", target)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
